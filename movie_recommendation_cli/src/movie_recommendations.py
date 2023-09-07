4e512373b8add362146bb8a0d7bce2c071512c4b
gilbert <gilbert.bageni@student.moringaschool.com>
2023-09-07 13:33:14 +0300

    updated code

--- movie_recommendation_cli/src/movie_recommendations.py
+++ movie_recommendation_cli/src/movie_recommendations.py
@@ -3,13 +3,11 @@ import requests
 from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from termcolor import colored  # Import termcolor for styled text
-import bcrypt  # For secure password hashing
+from termcolor import colored
+import bcrypt
 
-# Replace 'YOUR_API_KEY' with your actual TMDb API key
+# Constants
 API_KEY = '3fd2be6f0c70a2a598f084ddfb75487c'
-
-# Define the base URL for TMDb API
 BASE_URL = 'https://api.themoviedb.org/3'
 
 # SQLAlchemy setup
@@ -18,7 +16,7 @@ engine = create_engine('sqlite:///movie_db.sqlite')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Define the Movie table
+# Database Models
 class Movie(Base):
     __tablename__ = 'movies'
     id = Column(Integer, primary_key=True)
@@ -26,9 +24,8 @@ class Movie(Base):
     overview = Column(String)
     release_date = Column(String)
     vote_average = Column(Float)
-    recommended = Column(Boolean, default=False)  # Add a recommended column
+    recommended = Column(Boolean, default=False)
 
-# Define the User table
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
@@ -36,32 +33,26 @@ class User(Base):
     email = Column(String)
     password = Column(String)
 
+Base.metadata.create_all(engine)
+
+# User Authentication Functions
 def register_user():
-    # Simulate user registration
     username = input("Enter your username: ")
     email = input("Enter your email: ")
     password = input("Create a password: ")
-
-    # Hash the user's password before storing it
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-    # Check if the username already exists
-    existing_user = session.query(User).filter_by(username=username).first()
-    if existing_user:
+    if session.query(User).filter_by(username=username).first():
         print(colored("Username already exists. Please choose a different username.", 'red'))
     else:
-        # Create a new user
         new_user = User(username=username, email=email, password=hashed_password.decode('utf-8'))
         session.add(new_user)
         session.commit()
         print(colored(f"Welcome, {username}! Your account has been created successfully.", 'green'))
 
 def login_user():
-    # Simulate user login
     username = input("Enter your username: ")
     password = input("Enter your password: ")
-
-    # Retrieve the user's hashed password from the database
     user = session.query(User).filter_by(username=username).first()
 
     if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
@@ -71,27 +62,22 @@ def login_user():
         print(colored("Login failed. Please check your credentials.", 'red'))
         return False
 
+# Movie Search Functions
 def search_movies():
     if not login_user():
         print(colored("You must log in to search for movies.", 'red'))
         return
 
-    # Simulate searching for movies
     search_query = input("Enter a movie title or keyword to search: ")
-
-    # Define the endpoint for searching movies
     endpoint = '/search/movie'
-
-    # Define the query parameters
     params = {
         'api_key': API_KEY,
         'query': search_query,
     }
 
     try:
-        # Make an API request to search for movies
         response = requests.get(BASE_URL + endpoint, params=params)
-        response.raise_for_status()  # Raise an exception for HTTP errors
+        response.raise_for_status()
 
         movies = response.json()['results']
 
@@ -99,35 +85,37 @@ def search_movies():
             print(colored("No movies found for the given query.", 'yellow'))
             return
 
-        # Display movie search results
-        print(colored("Search Results:", 'cyan'))
-        for i, movie in enumerate(movies, start=1):
-            print(colored(f"{i}. {movie['title']} ({movie['release_date']})", 'yellow'))
-            print(colored(f"Overview: {movie['overview']}", 'magenta'))
-            print(colored(f"Vote Average: {movie['vote_average']}", 'green'))
-            print("---")
+        display_movie_search_results(movies)
 
     except requests.exceptions.RequestException as e:
         print(colored(f"Error fetching movie search results: {e}", 'red'))
 
+def display_movie_search_results(movies):
+    print(colored("Search Results:", 'cyan'))
+    for i, movie in enumerate(movies, start=1):
+        print(colored(f"{i}. {movie['title']} ({movie['release_date']})", 'yellow'))
+        print(colored(f"Overview: {movie['overview']}", 'magenta'))
+        print(colored(f"Vote Average: {movie['vote_average']}", 'green'))
+        print("---")
+
+# Rate Movie Function
 def rate_movie():
     if not login_user():
         print(colored("You must log in to rate a movie.", 'red'))
         return
 
-    # Simulate rating a movie
     movie_id = input("Enter the movie ID: ")
     rating = input("Rate the movie (1-10): ")
 
-    # Simulate rating confirmation (you can implement actual rating logic)
+    # Implement your logic to rate the movie here
     print(colored(f"Thank you for rating the movie! You gave it a rating of {rating} stars.", 'green'))
 
+# View Favorites Function
 def view_favorites():
     if not login_user():
         print(colored("You must log in to view your favorite movies.", 'red'))
         return
 
-    # Simulate retrieving and displaying user's favorite movies
     favorite_movies = session.query(Movie).filter_by(recommended=True).all()
 
     if not favorite_movies:
@@ -140,26 +128,26 @@ def view_favorites():
             print(colored(f"Vote Average: {movie.vote_average}", 'green'))
             print("---")
 
+# Add Recommendation Function
 def add_recommendation():
     if not login_user():
         print(colored("You must log in to add a recommended movie.", 'red'))
         return
 
-    # Simulate adding a recommended movie to favorites
     movie_name = input("Enter the name of the recommended movie: ")
 
-    # Simulate adding the movie to favorites (you can implement actual logic)
+    # Implement your logic to add the movie as a recommendation here
     new_movie = Movie(title=movie_name, recommended=True)
     session.add(new_movie)
     session.commit()
     print(colored(f"{movie_name} has been added to your favorites.", 'green'))
 
+# Get Recommendations Function
 def get_recommendations():
     if not login_user():
         print(colored("You must log in to get movie recommendations.", 'red'))
         return
 
-    # Simulate getting movie recommendations
     recommended_movies = session.query(Movie).filter_by(recommended=True).all()
 
     if not recommended_movies:
@@ -170,12 +158,12 @@ def get_recommendations():
             print(colored(f"{i}. {movie.title}", 'yellow'))
             print("---")
 
+# View Profile Function
 def view_profile():
     if not login_user():
         print(colored("You must log in to view your profile.", 'red'))
         return
 
-    # Simulate retrieving and displaying user's profile information
     username = input("Enter your username: ")
     user_info = session.query(User).filter_by(username=username).first()
 
@@ -186,33 +174,78 @@ def view_profile():
     else:
         print(colored("User not found.", 'red'))
 
+# Recommend Best Movies Function
+def recommend_best_movies():
+    if not login_user():
+        print(colored("You must log in to get movie recommendations.", 'red'))
+        return
+
+    best_movies = [
+        "Movie 1",
+        "Movie 2",
+        "Movie 3",
+        "Movie 4",
+    ]
+
+    print(colored("Recommended Best Movies:", 'cyan'))
+    for i, movie in enumerate(best_movies, start=1):
+        print(colored(f"{i}. {movie}", 'yellow'))
+        print("---")
+
+# Delete Favorite Function
+def delete_favorite():
+    if not login_user():
+        print(colored("You must log in to delete a movie from your favorites.", 'red'))
+        return
+
+    favorite_movies = session.query(Movie).filter_by(recommended=True).all()
+
+    if not favorite_movies:
+        print(colored("You haven't added any favorite movies yet.", 'yellow'))
+        return
+
+    print(colored("Your Favorite Movies:", 'cyan'))
+    for i, movie in enumerate(favorite_movies, start=1):
+        print(colored(f"{i}. {movie.title} ({movie.release_date})", 'yellow'))
+        print(colored(f"Overview: {movie.overview}", 'magenta'))
+        print(colored(f"Vote Average: {movie.vote_average}", 'green'))
+        print("---")
+
+    try:
+        movie_index = int(input("Enter the number of the movie to delete: ")) - 1
+
+        if 0 <= movie_index < len(favorite_movies):
+            movie_to_delete = favorite_movies[movie_index]
+
+            confirm = input(f"Are you sure you want to delete '{movie_to_delete.title}' from your favorites? (yes/no): ")
+
+            if confirm.lower() == 'yes':
+                session.delete(movie_to_delete)
+                session.commit()
+                print(colored(f"'{movie_to_delete.title}' has been deleted from your favorites.", 'green'))
+            else:
+                print(colored(f"'{movie_to_delete.title}' was not deleted.", 'yellow'))
+        else:
+            print(colored("Invalid movie number. No movie was deleted.", 'yellow'))
+
+    except ValueError:
+        print(colored("Invalid input. No movie was deleted.", 'yellow'))
+
+# Main Function
 def main():
     parser = argparse.ArgumentParser(description='Movie Recommendation CLI')
     subparsers = parser.add_subparsers(dest='command', help='Available commands')
 
-    # Create "register" subcommand
     register_parser = subparsers.add_parser('register', help='Register a new user')
-
-    # Create "login" subcommand
     login_parser = subparsers.add_parser('login', help='Log in as an existing user')
-
-    # Create "search" subcommand
     search_parser = subparsers.add_parser('search', help='Search for movies')
-
-    # Create "rate" subcommand
     rate_parser = subparsers.add_parser('rate', help='Rate a movie')
-
-    # Create "view-favorites" subcommand
     view_favorites_parser = subparsers.add_parser('view-favorites', help='View user\'s favorite movies')
-
-    # Create "add-recommendation" subcommand
     add_recommendation_parser = subparsers.add_parser('add-recommendation', help='Add a recommended movie to favorites')
-
-    # Create "get-recommendations" subcommand
     get_recommendations_parser = subparsers.add_parser('get-recommendations', help='Get movie recommendations')
-
-    # Create "view-profile" subcommand
     view_profile_parser = subparsers.add_parser('view-profile', help='View user\'s profile')
+    recommend_best_parser = subparsers.add_parser('recommend-best', help='Recommend the best movies')
+    delete_parser = subparsers.add_parser('delete', help='Delete a movie from favorites')
 
     args = parser.parse_args()
 
@@ -232,6 +265,10 @@ def main():
         get_recommendations()
     elif args.command == 'view-profile':
         view_profile()
+    elif args.command == 'recommend-best':
+        recommend_best_movies()
+    elif args.command == 'delete':
+        delete_favorite()
 
 if __name__ == '__main__':
     main()
